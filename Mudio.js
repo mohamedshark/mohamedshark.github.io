751a60dd4e7c1961f738d52e45cdcf88af7b2b9e
mohamedshark <midofaty87@gmail.com>
2024-07-07 02:04:27 +0300

    s

---
+++ Mudio.js
@@ -0,0 +1,10 @@
+function Showmenu(){
+    const x = document.querySelector('.mini-nav');
+    x.style.right='0px';x.style.display = 'block';
+}
+
+function Hidemenu(){
+    const x = document.querySelector('.mini-nav');
+    x.style.display = 'block'; x.style.right='-239px';
+
+}
